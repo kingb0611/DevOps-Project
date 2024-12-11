3764a882b338e6eef5ab36c23a1b280ca493c600
kingb0611 <154064991+kingb0611@users.noreply.github.com>
2024-12-11 18:58:09 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,19 +1,5 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+  ami = "ami-036841078a4b68e14"
   instance_type = "t2.micro"
   key_name = "ohiokey"
 
